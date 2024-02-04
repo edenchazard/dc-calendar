86eaf1eb9a44110f7b113f2ed3673f626a252f94
Charley Reeson <neon.fortytwo@gmail.com>
2024-02-04 17:38:57 +0000

    try not to rebase wrong challenge

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     outDir: './docs',
     emptyOutDir: true,
   },
+  base: '/dc-calendar/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
