c935fe517b2d098f3b59d0829cbfed7e98191127
Charley Reeson <neon.fortytwo@gmail.com>
2024-02-11 11:25:05 +0000

    drop vite multi config in favour of a single config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
+import { defineConfig, configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -14,6 +13,22 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    coverage: {
+      exclude: [
+        ...configDefaults.exclude,
+        'docs/*',
+        'playwright.config.ts',
+        'index.html',
+        '.eslintrc.cjs',
+        'env.d.ts',
+        'src/main.ts',
+      ],
     },
   },
 });
