13f7ee677610882da52a5d14027bdd32e4b17076
Charley Reeson <neon.fortytwo@gmail.com>
2024-03-02 17:38:34 +0000

    make separate branch for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: './docs',
+    outDir: './pages',
     emptyOutDir: true,
   },
   base: '/dc-calendar/',
