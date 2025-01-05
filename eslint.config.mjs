2488c5919dbc2d1a46308dcdc512ae0dd70697cc
Charley Reeson <neon.fortytwo@gmail.com>
2025-01-05 12:26:27 +0000

    ... lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
   rules: {
     'vue/html-self-closing': 0,
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 });
 // Your custom configs here
